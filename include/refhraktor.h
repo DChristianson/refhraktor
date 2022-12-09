619b742991f5b80fa726ffa4da410e4d96b07711
dchristianson <christianson.davidb@gmail.com>
2022-12-08 23:24:31 -0800

    prep for power experiments

--- include/refhraktor.h
+++ include/refhraktor.h
@@ -17,3 +17,79 @@
             lda #<{1}
             sta tx_on_timer
     ENDM
+
+    MAC GRID_TREATMENT_0
+           ; roll in from sides
+            ;         5 <B   23> 27<2B  43> 47<4B 53>57<5B 73 77<7B
+            ;  | 4..7 | 7......0 | 0......7 | 4..7 | 7......0 | 0......7 |
+            lda player_x,x
+            lsr
+            lsr
+            lsr
+            lsr
+            tay ; y is approx player location
+            sec
+            sbc #$0a
+            pha
+            lda player_state,x
+            and #PLAYER_STATE_FIRING
+            clc
+            eor #PLAYER_STATE_FIRING
+            beq _power_grid_skip_power
+            lda #$08
+            sec
+_power_grid_skip_power
+            php
+            ora power_grid_pf0,x
+            rol 
+            sta power_grid_pf0,x
+            dey
+            bmi _power_grid_right
+            ror power_grid_pf1,x
+            dey
+            dey
+            bmi _power_grid_right
+            rol power_grid_pf2,x
+            dey
+            dey
+            bmi _power_grid_right
+            lda #$00
+            bcc _power_grid_bridge_power_left
+            lda #$08
+_power_grid_bridge_power_left
+            ora power_grid_pf3,x
+            rol 
+            sta power_grid_pf3,x
+            dey
+            bmi _power_grid_right
+            ror power_grid_pf4,x
+_power_grid_right
+            plp
+            pla
+            tay
+            ror power_grid_pf5,x
+            iny
+            iny
+            bpl _power_grid_next
+            rol power_grid_pf4,x
+            iny
+            iny
+            bpl _power_grid_next
+            lda power_grid_pf3,x
+            and #$f0 ; BUGBUG could add power here
+            ror
+            sta power_grid_pf3,x
+            iny 
+            bpl _power_grid_next
+            and #$08
+            beq _power_grid_bridge_power_right
+            sec
+_power_grid_bridge_power_right
+            ror power_grid_pf2,x
+            iny
+            iny
+            bpl _power_grid_next
+            ror power_grid_pf1,x
+_power_grid_next
+
+    ENDM
--- refhraktor.asm
+++ refhraktor.asm
@@ -507,79 +507,16 @@ power_grid_update
             ldx #NUM_PLAYERS - 1
             
 _power_grid_update_loop
-            ; BUGBUG: TODO: other methods
-            ; roll in from sides
-            ;         5 <B   23> 27<2B  43> 47<4B 53>57<5B 73 77<7B
-            ;  | 4..7 | 7......0 | 0......7 | 4..7 | 7......0 | 0......7 |
-            lda player_x,x
-            lsr
-            lsr
-            lsr
-            lsr
-            tay ; y is approx player location
-            sec
-            sbc #$0a
-            pha
-            lda player_state,x
-            and #PLAYER_STATE_FIRING
-            clc
-            eor #PLAYER_STATE_FIRING
-            beq _power_grid_skip_power
-            lda #$08
-            sec
-_power_grid_skip_power
-            php
-            ora power_grid_pf0,x
-            rol 
-            sta power_grid_pf0,x
-            dey
-            bmi _power_grid_right
-            ror power_grid_pf1,x
-            dey
-            dey
-            bmi _power_grid_right
-            rol power_grid_pf2,x
-            dey
-            dey
-            bmi _power_grid_right
-            lda #$00
-            bcc _power_grid_bridge_power_left
-            lda #$08
-_power_grid_bridge_power_left
-            ora power_grid_pf3,x
-            rol 
-            sta power_grid_pf3,x
-            dey
-            bmi _power_grid_right
-            ror power_grid_pf4,x
-_power_grid_right
-            plp
-            pla
-            tay
-            ror power_grid_pf5,x
-            iny
-            iny
-            bpl _power_grid_next
-            rol power_grid_pf4,x
-            iny
-            iny
-            bpl _power_grid_next
-            lda power_grid_pf3,x
-            and #$f0 ; BUGBUG could add power here
-            ror
-            sta power_grid_pf3,x
-            iny 
-            bpl _power_grid_next
-            and #$08
-            beq _power_grid_bridge_power_right
-            sec
-_power_grid_bridge_power_right
-            ror power_grid_pf2,x
-            iny
-            iny
-            bpl _power_grid_next
-            ror power_grid_pf1,x
-_power_grid_next
+            ; TODO: other methods
+            ; TREATMENT 1: just track under player to figure out where player is
+            ; TREATMENT 2: flow in asymmetrically from sides based on power var, converge on player
+            ; TREATMENT 3: put in regular gaps as power drains 
+            ; TREATMENT 4: put in flow based gaps as power drains 
+            ; TREATMENT 5: adjust colors
+            ; TREATMENT 6: flicker as power drains
+            ; TREATMENT 7: flow left/right/center
+            ; TREATMENT 8: sound designs
+            GRID_TREATMENT_0
             dex
             bpl _power_grid_update_loop
 _power_grid_update_end
